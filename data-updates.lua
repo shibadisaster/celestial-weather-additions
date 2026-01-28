8a45bdd79250519bd007a9a9b6c8b13acc7b3ba3
shibadisaster <38396090+shibadisaster@users.noreply.github.com>
2026-01-28 19:18:08 +0800

    Add muria acid spore effect
    currently bugged and disappears slowly

--- data-updates.lua
+++ data-updates.lua
@@ -22,6 +22,46 @@ local fog = {
 }
 
 
+------ Direct Particles ------
+local direct_particles = {
+    type = "direct",
+    action_delivery = {
+        type = "instant",
+        source_effects = {
+            type = "create-trivial-smoke",
+            speed_multiplier_deviation = 0.2,
+            starting_frame = 512,
+            starting_frame_deviation = 1024,
+            offset_deviation = {{-96, -48}, {96, 48}},
+            speed_from_center = 0,
+            speed_from_center_deviation = 0
+        }
+    }
+}
+
+
+------ Cluster Particles ------
+local cluster_particles = {
+    type = "cluster",
+    distance = 8,
+    distance_deviation = 8,
+    action_delivery = {
+        type = "instant",
+        source_effects = {
+            type = "create-trivial-smoke",
+            initial_height = 0.6,
+            speed_multiplier_deviation = 0.2,
+            starting_frame = 512,
+            starting_frame_deviation = 1024,
+            offset_deviation = {{-96, -48}, {96, 48}},
+            speed_from_center = 0,
+            speed_from_center_deviation = 0
+        }
+    }
+}
+
+
+
 
 
 ------ Rubia ------
@@ -54,13 +94,13 @@ data.raw["planet"]["rubia"].player_effects = rubia_effects
 
 
 -- add overlay fog
--- local rubia_fog = table.deepcopy(fog)
--- rubia_fog.color1 = {0.822, 0.700, 0.564, 0.5}
--- rubia_fog.color2 = {0.822, 0.700, 0.564, 0.5}
--- rubia_fog.tick_factor = 0.003
--- rubia_fog.detail_noise_texture.filename = "__celestial-weather__/graphics/entity/dense-clouds.png"
+local rubia_fog = table.deepcopy(fog)
+rubia_fog.color1 = {0.822, 0.700, 0.564, 0.5}
+rubia_fog.color2 = {0.822, 0.700, 0.564, 0.5}
+rubia_fog.tick_factor = 0.003
+rubia_fog.detail_noise_texture.filename = "__celestial-weather__/graphics/entity/dense-clouds.png"
 
--- data.raw["planet"]["rubia"].surface_render_parameters["fog"] = rubia_fog
+data.raw["planet"]["rubia"].surface_render_parameters["fog"] = rubia_fog
 
 
 
@@ -95,11 +135,41 @@ muria_fog.detail_noise_texture.filename = "__celestial-weather__/graphics/entity
 data.raw["planet"]["muria"].surface_render_parameters["fog"] = muria_fog
 
 
+local muria_effects = table.deepcopy(data.raw["planet"]["muria"].player_effects)
+
+-- acid spores
+local muria_acid = table.deepcopy(data.raw["trivial-smoke"]["aquilo-snow-smoke"])
+muria_acid.name = "muria_acid"
+muria_acid.color = {0.83, 0.35, 0.52}
+muria_acid.animation.filename = "__celestial-weather__/graphics/entity/fire-particles.png"
+data:extend({muria_acid})
+
+local muria_weather_acid = table.deepcopy(direct_particles)
+muria_weather_acid.action_delivery.source_effects = {
+    muria_weather_acid.action_delivery.source_effects
+}
+muria_weather_acid.action_delivery.source_effects[1].smoke_name = "muria_acid"
+muria_weather_acid.action_delivery.source_effects[1].speed = {0.0, 0.0}
+muria_weather_acid.action_delivery.source_effects[1].speed_multiplier = 1.0
+muria_weather_acid.action_delivery.source_effects[1].repeat_count = 20
+
+-- muria_weather_acid.action_delivery.source_effects.smoke_name = "muria_acid"
+-- muria_weather_acid.action_delivery.source_effects.speed = {0.0, 0.0}
+-- muria_weather_acid.action_delivery.source_effects.speed_multiplier = 1.0
+-- muria_weather_acid.action_delivery.source_effects.repeat_count = 10
+
+-- implement
+data.raw["planet"]["muria"].ticks_between_player_effects = 1
+table.insert(muria_effects, muria_weather_acid)
+
+data.raw["planet"]["muria"].player_effects = muria_effects
+
+
 
 
 
 ------ Paracelsin ------
 local paracelsin_snow = data.raw["trivial-smoke"]["pa_snow"]
 paracelsin_snow.color = {0.188, 0.188, 0.188, 1.0}
-paracelsin_snow.start_scale = 1.5
-paracelsin_snow.end_scale = 1.5
+paracelsin_snow.start_scale = 0.8
+paracelsin_snow.end_scale = 0.8
