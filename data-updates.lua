84ddeb395b8701f6a5b08412a9a14d34061074ff
shibadisaster <38396090+shibadisaster@users.noreply.github.com>
2026-02-05 06:02:03 +0800

    Refactor Muria effects and add Muria dust

--- data-updates.lua
+++ data-updates.lua
@@ -298,8 +298,10 @@ if mods["Muria"] and settings.startup["shibadisaster-cwa-enable-muria-changes"].
 
     -- note to self/whoever is reading this: since muria already has an existing effect (rain), idk how to add my effect on top elegantly, if anyone knows how please leave something on the discussions page
     --     this was the first way i figured out to do this that worked, but refer to vesta effects to see a better way to do this
-    local muria_effects_action_delivery = {table.deepcopy(data.raw["planet"]["muria"].player_effects.action_delivery)}
+    -- local muria_effects_action_delivery = {table.deepcopy(data.raw["planet"]["muria"].player_effects.action_delivery)}
     local muria_effects = {table.deepcopy(data.raw["planet"]["muria"].player_effects)} or {}
+
+    if true then muria_effects = {} end
 
     -- acid spores
     if settings.startup["shibadisaster-cwa-muria-enable-spores"].value then
@@ -330,35 +332,35 @@ if mods["Muria"] and settings.startup["shibadisaster-cwa-enable-muria-changes"].
 
         local muria_weather_large_acid = table.deepcopy(direct_particles)
         muria_weather_large_acid.action_delivery.source_effects.smoke_name = "muria_large_acid_spore"
-        muria_weather_large_acid.action_delivery.source_effects.speed = {0.0, 0.0}
+        muria_weather_large_acid.action_delivery.source_effects.speed = {-0.05, 0.0}
         muria_weather_large_acid.action_delivery.source_effects.speed_multiplier = 5.0
         muria_weather_large_acid.action_delivery.source_effects.repeat_count = 1
         muria_weather_large_acid.action_delivery.source_effects.probability = 0.4
 
         local muria_weather_small_acid = table.deepcopy(direct_particles)
         muria_weather_small_acid.action_delivery.source_effects.smoke_name = "muria_small_acid_spore"
-        muria_weather_small_acid.action_delivery.source_effects.speed = {0.0, 0.0}
+        muria_weather_small_acid.action_delivery.source_effects.speed = {-0.07, 0.0}
         muria_weather_small_acid.action_delivery.source_effects.speed_multiplier = 5.0
         muria_weather_small_acid.action_delivery.source_effects.repeat_count = 1
         muria_weather_small_acid.action_delivery.source_effects.probability = 0.4
 
         local muria_weather_very_small_acid = table.deepcopy(direct_particles)
         muria_weather_very_small_acid.action_delivery.source_effects.smoke_name = "muria_very_small_acid_spore"
-        muria_weather_very_small_acid.action_delivery.source_effects.speed = {0.0, 0.0}
+        muria_weather_very_small_acid.action_delivery.source_effects.speed = {-0.09, 0.0}
         muria_weather_very_small_acid.action_delivery.source_effects.speed_multiplier = 5.0
         muria_weather_very_small_acid.action_delivery.source_effects.repeat_count = 1
         muria_weather_very_small_acid.action_delivery.source_effects.probability = 0.4
 
-        table.insert(muria_effects_action_delivery, muria_weather_large_acid.action_delivery)
-        table.insert(muria_effects_action_delivery, muria_weather_small_acid.action_delivery)
-        table.insert(muria_effects_action_delivery, muria_weather_very_small_acid.action_delivery)
+        table.insert(muria_effects, muria_weather_large_acid)
+        table.insert(muria_effects, muria_weather_small_acid)
+        table.insert(muria_effects, muria_weather_very_small_acid)
     end
 
 
     if settings.startup["shibadisaster-cwa-muria-enable-clouds"].value then
         local muria_cloud = table.deepcopy(data.raw["trivial-smoke"]["aquilo-snow-smoke"])
         muria_cloud.name = "muria_cloud"
-        muria_cloud.color = {0.0375, 0.05, 0.0, 0.1}
+        muria_cloud.color = {0.075, 0.1, 0.0, 0.15}
         muria_cloud.animation.filename = "__celestial-weather-additions__/graphics/vfx/cloud-a.png"
         muria_cloud.animation.frame_count = 1
         muria_cloud.animation.size = 512
@@ -376,11 +378,31 @@ if mods["Muria"] and settings.startup["shibadisaster-cwa-enable-muria-changes"].
         muria_weather_cloud.action_delivery.source_effects.repeat_count = 1
         muria_weather_cloud.action_delivery.source_effects.probability = 0.03
 
-        table.insert(muria_effects_action_delivery, muria_weather_cloud.action_delivery)
+        table.insert(muria_effects, muria_weather_cloud)
     end
 
+
+    if true then
+        local muria_dust = table.deepcopy(data.raw["trivial-smoke"]["aquilo-snow-smoke"])
+        muria_dust.name = "muria_dust"
+        muria_dust.color = settings.startup["shibadisaster-cwa-muria-spore-color"].value
+        muria_dust.start_scale = 1.0
+        muria_dust.end_scale = 1.0
+        muria_dust.animation.filename = "__celestial-weather__/graphics/entity/sand-particles.png"
+        data.extend({muria_dust})
+
+        local muria_weather_dust = table.deepcopy(cluster_particles)
+        muria_weather_dust.cluster_count = 2
+        muria_weather_dust.action_delivery.source_effects.smoke_name= "muria_dust"
+        muria_weather_dust.action_delivery.source_effects.speed = {-0.11, 0.0}
+        muria_weather_dust.action_delivery.source_effects.speed_multiplier = 1.0
+
+        table.insert(muria_effects, muria_weather_dust)
+    end
+
+
     data.raw["planet"]["muria"].ticks_between_player_effects = 1
-    data.raw["planet"]["muria"].player_effects.action_delivery = muria_effects_action_delivery
+    data.raw["planet"]["muria"].player_effects = muria_effects
 end
 
 
@@ -567,7 +589,7 @@ end
 if true then
     local rabbasca_dust = table.deepcopy(data.raw["trivial-smoke"]["aquilo-snow-smoke"])
     rabbasca_dust.name = "rabbasca_dust"
-    rabbasca_dust.color = {1.0, 0.5, 1.0}
+    rabbasca_dust.color = {0.4, 0.2, 0.4, 0.4}
     rabbasca_dust.start_scale = 2.0
     rabbasca_dust.end_scale = 2.0
     rabbasca_dust.animation.filename = "__celestial-weather__/graphics/entity/sand-particles.png"
